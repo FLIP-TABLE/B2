aa84e1cac564eda3c904a96aa4799582ffd86c77
Victor Hung <vhung@kth.se>
2016-06-11 13:42:54 +0200

    k-merge sort

--- sorter.cpp
+++ sorter.cpp
@@ -1,15 +1,9 @@
 #include <sys/resource.h>
 #include <iostream>
 #include <cstdlib>
+#include <climits>
 #include <fstream>
 #include <algorithm>
-
-#include "sys/types.h"
-#include "sys/sysinfo.h"
-
-#include "stdlib.h"
-#include "stdio.h"
-#include "string.h"
 
 const std::string random_name = "random_integers";
 const std::string sorted_name = "sorted_integers";
@@ -17,10 +11,11 @@ const int memsize = 10000;
 
 void generate(int N)
 {
+    srand(4500);
     std::ofstream outputfile(random_name);
     for(int i=0; i<N; ++i)
     {
-        outputfile << rand();
+        outputfile << rand() % (INT_MAX - 1);
         outputfile << "\n";
     }
     outputfile.close();
@@ -34,7 +29,8 @@ void external_sort()
     int i = 0;
     int k = 0;
     std::ifstream infile(random_name);
-
+    std::ofstream outfile(sorted_name);
+    // outfile.rd
     int x;
     for(infile >> x; !infile.eof(); infile >> x)
     {
@@ -61,7 +57,31 @@ void external_sort()
         savefiles[i] = std::ifstream();
         savefiles[i].rdbuf() -> pubsetbuf(0,0);
         savefiles[i].open("/tmp/temp-" + std::to_string(i));
+        savefiles[i] >> avail_memory[i];
     }
+
+    bool cont = true;
+    while(cont){
+        cont = false;
+        int index = 0;
+        for(i=0;i<k; ++i){
+            if(avail_memory[i] < avail_memory[index]){
+                index = i;
+            }
+        }
+        outfile << avail_memory[index] << std::endl;
+
+
+        savefiles[index] >> avail_memory[index];
+        if(savefiles[index].eof())
+            avail_memory[index] = INT_MAX;
+
+        for(i=0;i<k;++i){
+            if(!savefiles[i].eof())
+                cont = true;
+        }
+    }
+    outfile.close();
 }
 
 int main()
